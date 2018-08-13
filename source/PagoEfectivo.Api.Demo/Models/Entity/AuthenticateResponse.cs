079e90e956c576a79764a89fa2b36ef712ed5ac6
Paul Otiniano <paul.otiniano@orbis.com.pe>
2018-08-13 10:16:24 -0500

    feat:se agrega variable url

--- source/PagoEfectivo.Api.Demo/Models/Entity/AuthenticateResponse.cs
+++ source/PagoEfectivo.Api.Demo/Models/Entity/AuthenticateResponse.cs
@@ -15,4 +15,23 @@ namespace PagoEfectivo.Api.Demo.Models.Entity
         public string url { get; set; }
 
     }
+
+    public class GenerateResponse
+    {
+        public int Code { get; set; }
+
+        public string Message { get; set; }
+
+        public GenerateCip Data { get; set; }
+
+    }
+
+    public class GenerateCip
+    {
+        public Int64 Cip { get; set; }
+        public string Currency { get; set; }
+        public decimal amount { get; set; }
+        public string TransactionCode { get; set; }
+        public string CipUrl { get; set; }
+    }
 }
